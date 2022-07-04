3bddeafbb6fab4fda513a7205976053fa95e7841
Denisa <denisaicirstea@gmail.com>
2022-07-04 16:40:54 +0300

    my first project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <NavBar />
+
+    <router-view />
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
 
 <style lang="scss">
 #app {
@@ -15,18 +25,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="nav-bar">
+    <div class="logo">
+      <img alt="logo" src="../assets/logo.png">
+
+    </div>
+    <div class="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/blog">Blog</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+img {
+  width: 30px;
+
+}
+
+.nav-bar {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 12px 24px;
+
+  :hover {
+    color: red;
+
+
+  }
+}
+
+a {
+  text-decoration: none;
+  margin-left: 15px;
+
+  :hover {
+    color: red;
+
+
+
+  }
+}
+</style>
