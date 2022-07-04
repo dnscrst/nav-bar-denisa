993d33aff65b5621e35dab66d896e420ee2208a4
Denisa <denisaicirstea@gmail.com>
2022-07-04 17:15:42 +0300

    something is wrong with axios get

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,39 @@
 <template>
     <div class="about">
         <h1>This is a blog page</h1>
+        <ul>
+            <li v-for="post in posts">
+                <h2>
+                    <span>{{ post.id }}.&nbsp;</span>
+                    <router-link :to="`/post/${post.id}`">{{ post.title }}</router-link>
+                </h2>
+                <p>{{ post.body }}</p>
+            </li>
+        </ul>
     </div>
 </template>
+
+<script>
+import axios from "axios"
+export default {
+    name: 'Blog',
+    data() {
+        return {
+            posts: []
+        }
+    },
+    created() {
+        const url = 'https://jsonplaceholder.typicode.com/'
+        axios.get(url)
+            .then(resp => {
+                this.posts = resp.data
+            })
+            .catch(e => {
+                console.log(e.response ? e.response.data : e)
+            })
+
+
+    }
+}
+
+</script>
